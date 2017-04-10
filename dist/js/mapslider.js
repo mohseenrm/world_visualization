ca3e609feb53d8b35590c08e1bee2ca045134456
Nagarchith <nagarchith@gmail.com>
2017-04-10 16:17:48 -0700

    Add files via upload

--- dist/js/mapslider.js
+++ dist/js/mapslider.js
@@ -4,10 +4,14 @@
 
 
 var obj = new Object();
-obj.year = 1961;
+obj.year = 1970;
 obj.fsi  = true;
 obj.gdp  = false;
-obj.filt3  = false;
+obj.inf  = false;
+obj.gini  = false;
+obj.popu  = false;
+obj.emp  = false;
+obj.res  = false;
 
 /*$('#range').on("change", function() {
     $('.output').val(this.value );
@@ -21,8 +25,8 @@ obj.filt3  = false;
 $(".slider")
 
     .slider({
-        min: 1960,
-        max: 2016,
+        min: 1970,
+        max: 2014,
         step: 1,
         slide: function( event, ui ) {
             //$( "#year_selected" ).val( ui.value);
@@ -40,42 +44,141 @@ $(".slider")
     .slider("float");
 
 $('input[type=checkbox]').change(function(){
-    counter = 0;
     clicked = $(this).data('index');
-    // alert(clicked)
+     //alert(clicked)
      if(clicked == 0 ){
-         obj.fsi=true;
-         obj.gdp=false;
-         obj.filt3=false;
-         document.getElementById("fast").checked = false;
-         document.getElementById("quality").checked = false;
+         obj.fsi  = true;
+         obj.gdp  = false;
+         obj.inf  = false;
+         obj.gini  = false;
+         obj.popu  = false;
+         obj.emp  = false;
+         obj.res  = false;
+         document.getElementById("GDP").checked = false;
+         document.getElementById("Inf").checked = false;
+         document.getElementById("Gini").checked = false;
+         document.getElementById("cheap").checked = false;
+         document.getElementById("Emp").checked = false;
+         document.getElementById("Reserve").checked = false;
          PostData()
      }
     if(clicked == 1 ){
-        obj.fsi=false;
-        obj.gdp=true;
-        obj.filt3=false;
+        obj.fsi  = false;
+        obj.gdp  = true;
+        obj.inf  = false;
+        obj.gini  = false;
+        obj.popu  = false;
+        obj.emp  = false;
+        obj.res  = false;
+        document.getElementById("FSI").checked = false;
+        document.getElementById("Inf").checked = false;
+        document.getElementById("Gini").checked = false;
         document.getElementById("cheap").checked = false;
-        document.getElementById("quality").checked = false;
+        document.getElementById("Emp").checked = false;
+        document.getElementById("Reserve").checked = false;
         PostData()
     }
     if(clicked == 2 ){
-        obj.fsi=false;
-        obj.gdp=false;
-        obj.filt3=true;
-        document.getElementById("fast").checked = false;
+        obj.fsi  = false;
+        obj.gdp  = false;
+        obj.inf  = true;
+        obj.gini  = false;
+        obj.popu  = false;
+        obj.emp  = false;
+        obj.res  = false;
+        document.getElementById("FSI").checked = false;
+        document.getElementById("GDP").checked = false;
+        document.getElementById("Gini").checked = false;
         document.getElementById("cheap").checked = false;
+        document.getElementById("Emp").checked = false;
+        document.getElementById("Reserve").checked = false;
+        PostData()
+    }
+    if(clicked == 3 ){
+        obj.fsi  = false;
+        obj.gdp  = false;
+        obj.inf  = false;
+        obj.gini  = true;
+        obj.popu  = false;
+        obj.emp  = false;
+        obj.res  = false;
+        document.getElementById("FSI").checked = false;
+        document.getElementById("GDP").checked = false;
+        document.getElementById("Inf").checked = false;
+        document.getElementById("cheap").checked = false;
+        document.getElementById("Emp").checked = false;
+        document.getElementById("Reserve").checked = false;
+        PostData()
+    }
+    if(clicked == 4 ){
+        obj.fsi  = false;
+        obj.gdp  = false;
+        obj.inf  = false;
+        obj.gini  = false;
+        obj.popu  = true;
+        obj.emp  = false;
+        obj.res  = false;
+        document.getElementById("FSI").checked = false;
+        document.getElementById("GDP").checked = false;
+        document.getElementById("Gini").checked = false;
+        document.getElementById("Inf").checked = false;
+        document.getElementById("Emp").checked = false;
+        document.getElementById("Reserve").checked = false;
+        PostData()
+    }
+    if(clicked == 5 ){
+        obj.fsi  = false;
+        obj.gdp  = false;
+        obj.inf  = false;
+        obj.gini  = false;
+        obj.popu  = false;
+        obj.emp  = true;
+        obj.res  = false;
+        document.getElementById("FSI").checked = false;
+        document.getElementById("GDP").checked = false;
+        document.getElementById("Gini").checked = false;
+        document.getElementById("cheap").checked = false;
+        document.getElementById("Inf").checked = false;
+        document.getElementById("Reserve").checked = false;
+        PostData()
+    }
+    if(clicked == 6 ){
+        obj.fsi  = false;
+        obj.gdp  = false;
+        obj.inf  = false;
+        obj.gini  = false;
+        obj.popu  = false;
+        obj.emp  = false;
+        obj.res  = true;
+        document.getElementById("FSI").checked = false;
+        document.getElementById("GDP").checked = false;
+        document.getElementById("Gini").checked = false;
+        document.getElementById("cheap").checked = false;
+        document.getElementById("Emp").checked = false;
+        document.getElementById("Inf").checked = false;
         PostData()
     }
 
 });
 
 function PostData(){
-    $post("https://localhost:9001/main",JSON.stringify(obj),"","json");
+   // $.post("https://localhost:9001/main",JSON.stringify(obj),"","json");
 
     // Or this
     // var bad = JSON.stringify(obj) + " 0\r\n";
     // var x = new XMLHttpRequest();
     // x.open("POST", "http://localhost:6379");
     // x.send(bad);
+
+    $.ajax({
+        type: 'POST',
+        url: "https://localhost:9001/main",
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify(obj)
+    })
+        .done(function( msg ) {
+            alert( "Data Saved: " + msg );
+        });
+
 }
